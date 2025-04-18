cb659d5c24c8a8c4c730efc4175b21901607784a
Muhammad Muflih <121600789+MuflihItsuka312@users.noreply.github.com>
2025-04-18 03:12:32 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoose = require("mongoose");
 
+// const DB_URI = 'mongodb://localhost:27017/manga'; //fix
 
 const app = express();
-const PORT = 3000;
-const DB_URI = 'mongodb://localhost:27017/mydatabase'; // Replace with your MongoDB URI
+// const PORT = process.env.PORT || 3000;
 
+//set template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -14,6 +17,32 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+//route prefix
+app.use('',require('./router/routes'));
+
+//Connect to MongoDB
+mongoose.connect(process.env.DB_URI, 
+  {
+    useNewUrlParser: true,
+     useUnifiedTopology: true,
+    }
+  );
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+//middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
